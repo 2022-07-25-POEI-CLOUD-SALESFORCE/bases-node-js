8adda2815eab38517f32593be4331e17be3c9e37
Christian Lisangola <jochri3@gmail.com>
2022-09-01 12:43:25 +0300

    manager route handler

--- 2-modules-natifs/http-module.js
+++ 2-modules-natifs/http-module.js
@@ -17,14 +17,25 @@ const database = {
 
 // url : /api/players
 // url : /api/managers
+// url de base : Afficher welcome
 
 const server = http.createServer((request, response) => {
+  if (request.url === "/") {
+    response.end("Welcome");
+  }
   if (request.url === "/api/players") {
     response.writeHead(200, { "Content-Type": "application/json" });
-    response.end(database.players);
+    response.end(JSON.stringify(database.players));
   }
   if (request.url === "/api/managers") {
+    response.writeHead(200, { "Content-Type": "application/json" });
+    response.end(JSON.stringify(database.managers));
   }
+});
+
+const PORT = 3000;
+server.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
 });
 
 // Status code
